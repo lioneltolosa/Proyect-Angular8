9865612c935c2bc60c0089f752bceedce6632030
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-01-17 11:07:45 +0100

    Implement Operador Map() and Operator Filter()

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, interval, throwError, of } from 'rxjs';
-import { mergeMap, retry } from 'rxjs/operators';
+import { Observable, interval, throwError, of, Subscription, Subscriber } from 'rxjs';
+import { mergeMap, retry, map, filter } from 'rxjs/operators';
 
 @Component({
     selector: 'app-rxjs',
@@ -10,9 +10,12 @@ import { mergeMap, retry } from 'rxjs/operators';
 export class RxjsComponent implements OnInit {
 
     constructor() {
-        this.returnObservable().pipe(
+        this.returnObservable()
+        
+        /* .pipe(
             retry(2)
-        )
+        ) */
+
         .subscribe( 
             number => console.log('Subscription', number),    // next()
             error => console.error('Error en el obs', error), // error
@@ -23,24 +26,42 @@ export class RxjsComponent implements OnInit {
     ngOnInit() {
     }
 
-    returnObservable(): Observable<number>{
-        return new Observable(observer => {
+    returnObservable(): Observable<any>{
+        return new Observable((observer : Subscriber<any>) => {
             let contador = 0;
             
             let intervalo = setInterval(() => {
                 contador ++;
-                observer.next( contador );
+
+                const salida = {
+                    valor: contador
+                }
+
+                observer.next(salida);
 
                 if(contador === 3) {
                     clearInterval(intervalo)
                     observer.complete();
                 }
 
-                if(contador === 2) {
+                /* if(contador === 2) {
                     observer.error('Help!!');
-                }
+                } */
             }, 1000)
-        });
+        }).pipe(
+            map( resp => resp.valor),
+            filter( (value, index) => {
+                /* console.log('value', value);
+                console.log('index', index);
+                return true; */
+
+                if( (value % 2) === 1 ) {
+                    return true
+                } else {
+                    return false
+                }
+            })
+        )
     }
 
 }
